ab5be3d92d3e067f5b442d488b970be690bfa4d6
taoxin <taoxin0704@gmail.com>
2021-06-10 19:15:24 +0800

    添加ffmpeg

--- src/MyFFMpeg.php
+++ src/MyFFMpeg.php
@@ -9,21 +9,10 @@ use FFMpeg\FFMpeg;
 class MyFFMpeg
 {
     private FFMpeg $ffmpeg;
-    private array $config;
 
     public function __construct($config = [])
     {
-        $this->setFfmpeg();
-        if (!$config) {
-            $this->config = [
-                'ffmpeg.binaries' => '/opt/local/ffmpeg/bin/ffmpeg',
-                'ffprobe.binaries' => '/opt/local/ffmpeg/bin/ffprobe',
-                'timeout' => 3600, // The timeout for the underlying process
-                'ffmpeg.threads' => 12,   // The number of threads that FFMpeg should use
-            ];
-        }else{
-            $this->config = $config;
-        }
+        $this->setFfmpeg($config);
     }
 
 
@@ -37,8 +26,8 @@ class MyFFMpeg
 
     /**
      */
-    private function setFfmpeg(): void
+    private function setFfmpeg($config): void
     {
-        $this->ffmpeg = FFMpeg::create($this->config);
+        $this->ffmpeg = FFMpeg::create($config);
     }
 }
